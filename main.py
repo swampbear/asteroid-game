9a7aaf7ce639e198b097baa7aa60e19f1c2a68bd
Jobjorn <672641@stud.hvl.no>
2025-09-19 18:30:58 +0200

    added 'wasd' compatibale player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -8,13 +9,28 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         screen.fill("black")
-
+        # Listens for user to close window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # CLock for adjusting FPS
+        clock = pygame.time.Clock()
+        updatable.update(dt)
+        for item  in drawable:
+            item.draw(screen)
 
+
+        dt = clock.tick(60)/1000
         pygame.display.flip()
 if __name__ == "__main__":
     main()
